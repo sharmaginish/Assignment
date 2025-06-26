dab094403d7b3131c0e9a1bd408369c7ccc0f724
sharmaginish <ginishsharma2005@gmail.com>
2025-06-26 09:43:11 +0530

    Add files via upload

---
+++ internship.py
@@ -0,0 +1,5 @@
+lunch_items = ["paneer butter masala", "butter naan", "lassi", "gulab jamun", "jeera rice"]
+print(lunch_items)
+print("Total number of items ordered:", len(lunch_items))
+lunch_items.append("papad")
+print(lunch_items)
